8a4d87a9a8604bff80b61408b43e1005479c618d
caio-faustino <caioexfaustino.pro@gmail.com>
2022-09-27 19:16:59 -0300

    requisite 05

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (names) => {
+  let student = {
+    name: names,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return student;
+};
 
 module.exports = createStudent;
